857c89424605fffa21dde77abb386f2045e24248
hung <hcaelb123@gmail.com>
2017-12-10 14:26:10 +0700

    server

---
+++ app_api/controller/userlogs.js
@@ -0,0 +1,16 @@
+const db = require('../model/db');
+const ACTIONS_ID = {
+
+}
+
+const create = function(bookId, userId, actionId) {
+}
+
+const delete = function(bookId, userId, actionId) {
+
+}
+
+module.exports = {
+	ACTIONS_ID,
+	create
+}
---
+++ app_api/model/db.js
@@ -0,0 +1,59 @@
+'use strict';
+const mysql = require('mysql');
+
+let pool;
+if (process.env.NODE_ENV === 'test') {
+	pool = mysql.createPool({
+		connectionLimit : 10,
+	  	host     : process.env.db_host_test,
+	  	user     : process.env.db_user_test,
+	  	password : process.env.db_password_test,
+	  	database : process.env.db_database_test
+	});
+} else if (process.env.NODE_ENV === 'production') {
+	const db_url = new URL(process.env.CLEARDB_DATABASE_URL);
+	pool = mysql.createPool({
+		connectionLimit : 10,
+	  	host     : db_url.host,
+	  	user     : db_url.username,
+	  	password : db_url.password,
+	  	database : db_url.path
+	});	
+} else {
+	pool = mysql.createPool({
+		connectionLimit : 10,
+	  	host     : process.env.db_host_dev,
+	  	user     : process.env.db_user_dev,
+	  	password : process.env.db_password_dev,
+	  	database : process.env.db_database_dev
+	});
+}
+
+const gracefulShutdown = function (msg, callback) {
+	pool.end(function(err) {
+		console.log(msg);
+		callback();
+	})
+};
+
+// For nodemon restarts                                 
+process.once('SIGUSR2', () => {
+	gracefulShutdown('nodemon restart', () => {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+// For app termination
+process.on('SIGINT', () => {
+	gracefulShutdown('app termination', () => {
+    	process.exit(0);
+  	});
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+  	gracefulShutdown('Heroku app shutdown', () => {
+    	process.exit(0);
+  	});
+});
+
+
+module.exports = pool;
